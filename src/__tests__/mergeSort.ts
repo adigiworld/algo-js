0e7320512a4c35027ebd7e92f7fce3c0b555b64c
Aadil Rasheed <asadigiworld@gmail.com>
2025-03-13 17:00:46 +0530

    Add: Merge-Sort

--- src/__tests__/mergeSort.ts
+++ src/__tests__/mergeSort.ts
@@ -2,20 +2,22 @@ import { mergeSort } from "../mergeSort";
 
 test("Merge Sort Algo", () => {
 
-  const arr = [9, 3, 12, 7, 4, 6, 2, 1, 8, 5, 11, 10];
-  const a = mergeSort(arr);
-  expect(a).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  //const arr = [9, 3, 12, 7, 4, 6, 2, 1, 8, 5, 11, 10];
+  //const a = mergeSort(arr);
+  //expect(a).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
   // console.log(arr);
-  // const arr = [9, 3, 12, 7, 4, 6, 2, 1, 8, 5, 11, 10];
-  // mergeSort(arr);
-  // expect(arr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  let arr = [9, 3, 12, 7, 4, 6, 2, 1, 8, 5, 11, 10];
+  mergeSort(arr);
+  //console.log(arr);
+  expect(arr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 
-  // const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  // insertionSort(arr1);
-  // expect(arr1).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-  // console.log(array);
+  const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  mergeSort(arr1);
+  expect(arr1).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 
-  // const arr2 = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-  // insertionSort(arr2);
-  // expect(arr2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const arr2 = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+  mergeSort(arr2);
+  expect(arr2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
+  console.log(arr2);
+
 });
--- src/mergeSort.ts
+++ src/mergeSort.ts
@@ -1,29 +1,51 @@
+export function mergeSort(list: number[]): void {
+  sort(list, 0, list.length - 1);
+}
+function sort(arr: number[], low: number, hig: number): void {
+  if (low >= hig) { return; }
+  const midIdx = low + Math.floor((hig - low) / 2);
+  sort(arr, low, midIdx);
+  sort(arr, midIdx + 1, hig);
 
-export function mergeSort(array: number[]): void {
-  const mid = Math.floor(array.length / 2);
+  merge(arr, low, midIdx, hig);
+}
+function merge(arr: number[], low: number, mid: number, hig: number): void {
+  const leftLength = (mid - low) + 1;
+  const rightLength = hig - mid;
+  let sortedLeft: number[] = [];
+  let sortedRight: number[] = [];
+  for (let i = 0; i < leftLength; i += 1) {
+    sortedLeft.push(arr[low + i]);
+  }
+  for (let j = 0; j < rightLength; j += 1) {
+    sortedRight.push(arr[mid + 1 + j]);
+  }
+  let l = 0;
+  let r = 0;
+  let k = low;
+  while (l < leftLength && r < rightLength) {
+    if (sortedLeft[l] < sortedRight[r]) {
+      arr[k] = sortedLeft[l];
+      l += 1;
+    } else {
+      arr[k] = sortedRight[r];
+      r += 1;
+    }
+    k += 1;
+  }
+  while (l < leftLength) {
+    arr[k] = sortedLeft[l];
+    l += 1;
+    k += 1;
+  }
+  while (r < rightLength) {
+    arr[k] = sortedRight[r];
+    r += 1;
+    k += 1;
+  }
 }
 
+//const a = [5, 4, 3, 2, 1];
+//mergeSort(a);
+//console.log(a);
 
-// export function mergeSort(array: number[]): number[] {
-//   if (array.length < 2) { return array; }
-//   const mid = Math.floor(array.length / 2);
-//   const left = mergeSort(array.slice(0, mid));
-//   const right = mergeSort(array.slice(mid));
-//   return merge(left, right);
-// }
-//
-// function merge(sortedLeft: number[], sortedRight: number[]): number[] {
-//   const result = [];
-//   while (sortedLeft.length > 0 && sortedRight.length > 0) {
-//     if (sortedLeft[0] <= sortedRight[0]) {
-//       // result.push(sortedLeft.shift());
-//       const item = sortedLeft.shift();
-//       if (item !== undefined) { result.push(item); }
-//     } else {
-//       // result.push(sortedRight.shift());
-//       const item = sortedRight.shift();
-//       if (item !== undefined) { result.push(item); }
-//     }
-//   }
-//   return result.concat(sortedLeft, sortedRight);
-// }
